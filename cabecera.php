661fa33d0525c5ae51654320f48b10088c973346
shankara83 <robertoarcoya@gmail.com>
2013-03-11 22:49:41 +0100

    Create cabecera.php

---
+++ cabecera.php
@@ -0,0 +1,25 @@
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
+<link href="css/estiloMenu.css" rel="stylesheet" type="text/css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<!-- inicio de cabecera -->
+
+ 
+ <body>
+        <div id="cabecera">                    
+            <h1>HOTEL CONFONRT </h1>
+            <ul id="menu">
+                <li> <a href="index.php">INICIO</a> </li>
+                <li> <a href="habitaciones.php">HABITACIONES</a> </li>
+                <li> <a href="reservas.php">RESERVAS</a> </li>
+                <li> <a href="consultas.php">CONSULTA DE RESERVAS</a> </li>
+            
+            </ul>
+            
+            
+        </div>
+    </body>
+  
+<!-- final de cabecera -->
