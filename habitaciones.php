b96ab2eda9b3d5a79b3615f1ef21bcc3e2a203d1
shankara83 <robertoarcoya@gmail.com>
2013-03-11 22:47:39 +0100

    Create habitaciones.php

---
+++ habitaciones.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Habitaciones</title>
+<?php
+  
+	include('cabecera.php');
+?>
+</head>
+
+
+ <body>
+ <div id="contenedor">
+       		
+            <section>
+            <!--imagen pequeña-->
+		  <ul class="lb-album">
+          				<li >
+						<a href="#image-1"><span>Sencilla</span><img  src="fotos/individualp.jpg" alt="sencilla" width="152" height="152">
+						</a>
+							
+                            <div class="lb-overlay" id="image-1">
+							<a href="#page" class="lb-close">Cerrar</a>
+							
+                            <img src="fotos/individual.jpg" alt="doble2" />
+                                <div>
+                                    <h3>Hotel Confort</h3>
+                                    <p>Habitación sencilla para un huesped. Televisión de pago. Cuarto de baño con bañera. Desayuno incluido.</p>
+                                </div>							
+					  		</div>
+					</li>
+					<li>
+                      <!--imagen grande al pulsar-->
+						<a href="#image-2"><span>Dos camas</span><img  src="fotos/doble2p.jpg" alt="image01" width="152" height="152">
+						</a>
+<div class="lb-overlay" id="image-2">
+							<a href="#page" class="lb-close">Cerrar</a>
+							<img src="fotos/doble2.jpg" alt="doble2" />
+							<div>
+								<h3>Hotel Confort</h3>
+								<p>Sencilla habitación para dos. Televisión de pago. Cuarto de baño con bañera. Desayuno incluido.</p>
+</div>							
+					  </div>
+					</li>
+					
+  <li>
+						<a href="#image-3">
+							<img src="fotos/doblep.jpg"alt="doblep" width="149" height="150">
+							<span>Doble</span>
+						</a>
+						<div class="lb-overlay" id="image-3">
+							<img src="fotos/doble3.jpg" alt="doble3" />
+							<div>							
+								<h3>Hotel Confort</h3>
+								<p>Excelente habitación doble. Televisión de pago. Cuarto de baño con bañera. Desayuno incluido. </p>
+							</div>
+							<a href="#page" class="lb-close">Cerrar</a>
+						</div>
+					</li>
+					<li>
+						<a href="#image-4">
+						<img src="fotos/triplep.jpg" alt="image03">
+						<span>Triple</span>
+						</a>
+						<div class="lb-overlay" id="image-4">
+						  <img src="fotos/triple.jpg"alt="triple" />
+							<div>							
+								<h3>Hotel Confort</h3>   
+                                <p>Amplia habitación triple. Televisión de pago. Cuarto de baño con bañera. Desayuno incluido.</p>
+						  </div>
+							<a href="#page" class="lb-close">Cerrar</a>
+						</div>
+					</li>
+				</ul>
+			</section>
+    
+      
+     
+      <div class="empuje"></div>
+      
+  </div>   
+         
+ 
+         <?php
+    		 include('pie.php');
+		?>
+</body>
+    
